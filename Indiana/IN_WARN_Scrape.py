33df2b33b9ddeb88078163d5feb9de593c377d19
Aiden Ettlinger <aidenettlinger@Aidens-MacBook-Pro.local>
2020-10-14 13:36:02 -0400

    Parse out company names for Indiana

--- Indiana/IN_WARN_Scrape.py
+++ Indiana/IN_WARN_Scrape.py
@@ -6,9 +6,11 @@ import re
 class Site:
     def __init__(self):
         self.full_table = None
-        self.page_url = "http://reactwarn.floridajobs.org/WarnList/Records?year=2020&page="
+        #If there are multiple pages be sure to have the link with 'page='' in it and remove the page number.
+        self.page_url = "https://www.in.gov/dwd/2567.htm"
         self.start_page = 1
-        self.total_pages = 13
+        self.total_pages = 1
+        self.multi_page = False
 
     def get_site_data(self):
         site_data_list = []
@@ -16,12 +18,16 @@ class Site:
         total_pages = self.total_pages
         starting_page = self.start_page
 
-        while starting_page <= total_pages:
-            pages_url = start_page_url + str(starting_page)
-            soup = self.get_soup(pages_url)
-            site_data_list = site_data_list + self.get_rows_from_page(soup)
-            print("Got data from website page " + str(starting_page))
-            starting_page += 1
+        if self.multi_page == True:
+            while starting_page <= total_pages:
+                pages_url = start_page_url + str(starting_page)
+                soup = self.get_soup(pages_url)
+                site_data_list = site_data_list + self.get_rows_from_page(soup)
+                print("Got data from website page " + str(starting_page))
+                starting_page += 1
+        else:
+            soup = self.get_soup(self.page_url)
+            site_data_list = self.get_rows_from_page(soup)
 
         self.full_table = site_data_list
 
@@ -32,14 +38,14 @@ class Site:
 
     def get_rows_from_page(self, soup):
         all_rows = []
-        table_rows = soup.tbody.find_all('tr')
+        table_rows = soup.find_all('tr')
         for row in table_rows:
             table_data = row.find_all('td')
             all_rows.append(table_data)
         return all_rows
 
     def print_site_data(self):
-        for row in self.table:
+        for row in self.full_table:
             print(row)
 
 class MyTable:
@@ -66,35 +72,46 @@ class MyTable:
         for row in self.raw_table:
             print(row)
 
+    def print_column(self, column):
+        for row in column:
+            print(row)
+
     def clean_column_company(self):
         company_column_raw = self.get_column(self.company_column_index)
-        #remove first element from company column. This is the column name and not a company name so must be removed.
         company_column_clean = []
-        location_clean = []
+
+        def clean_title(link):
+            name_start = link.rfind(">")
+            link = link[name_start + 1:]
+            return link
+
         for row in company_column_raw:
-            split_list = str(row).split('>')
-            #The company name is always the third element in the list after a split along the > part of the html syntax.
-            company_name = split_list[2]
-            company_name = company_name.replace('</b', '')
-            company_name = company_name.replace('&amp;', '&')
-            company_column_clean.append(company_name)
-            #Getting the location. Might as well since it appears in the same data cell as the company name.
-            address = []
-            address_line_one = split_list[4]
-            address_line_two = split_list[5]
-            address_line_three = split_list[6]
-            address_line_one = address_line_one.replace('<br/', '')
-            address_line_two = address_line_two.replace('<br/', '')
-            address_line_three = address_line_three.replace('</td', '')
-            address.append(address_line_one)
-            #2nd address line is optional. It won't be part of the address if it doesn't exist.
-            if address_line_two != '':
-                address.append(address_line_two)
-            address.append(address_line_three)
-            #Join individual address lines into one address string.
-            location_clean.append('\n'.join(address))
+            string_row = str(row)
+            if "<p>" in string_row:
+                name_start = string_row.find("<p>")
+                name_end = string_row.find("</p>")
+                name = string_row[name_start + 3:name_end]
+                if "<br/>" in name:
+                    name_end = name.find("<br/>")
+                    name = name[:name_end]
+                if "href" in name:
+                    name_start = name.find('"_self">')
+                    name_end = name.find("</a>")
+                    name = name[name_start + 8:name_end]
+                if "title" in name:
+                    name = clean_title(name)
+            else:
+                name_start = string_row.find('"_self">')
+                name_end = string_row.find("</a>")
+                name = string_row[name_start + 8:name_end]
+                if "title" in name:
+                    name = clean_title(name)
+            name = name.replace("&amp;", "&")
+            company_column_clean.append(name)
+
         self.company_column = company_column_clean
-        self.location_column = location_clean
+        for row in self.company_column:
+            print(row)
 
     def clean_column_effective_date(self):
         effective_date_column_raw = self.get_column(self.effective_date_column_index)
@@ -121,7 +138,10 @@ class MyTable:
     def get_column(self, column_index):
         column = []
         for row in self.raw_table:
-            column.append(row[column_index])
+            try:
+                column.append(row[column_index])
+            except:
+                print("Row could not be added to column")
         return column
 
     def create_final_table(self):
@@ -147,11 +167,14 @@ class CSVMaster:
             for row in self.table.final_table:
                 warn_writer.writerow(row)
 
-fl_site = Site()
-fl_site.get_site_data()
-fl_table = MyTable(fl_site.full_table)
-fl_table.clean_column_company()
-fl_table.clean_data()
-fl_table.create_final_table()
-fl_csv = CSVMaster(fl_table)
-fl_csv.create_csv_file()
+in_site = Site()
+in_site.get_site_data()
+#in_site.print_site_data()
+in_table = MyTable(in_site.full_table)
+in_table.clean_column_company()
+#fl_table = MyTable(fl_site.full_table)
+#fl_table.clean_column_company()
+#fl_table.clean_data()
+#fl_table.create_final_table()
+#fl_csv = CSVMaster(fl_table)
+#fl_csv.create_csv_file()
